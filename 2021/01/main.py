a1f468ea9c677cbd7e25871c2fecc4e78079b4d7
Sebastian Law <sebnlaw@gmail.com>
2021-12-01 21:38:11 -0800

    day 1 and 2

---
+++ 2021/01/main.py
@@ -0,0 +1,24 @@
+from util import read
+
+def sol1(inp):
+    x = 0
+    for i, n in enumerate(inp):
+        if n > inp[i - 1]:
+            x += 1
+
+    return x
+
+def sol2(inp):
+    windows = []
+    for i, n in enumerate(inp):
+        if i == len(inp) - 2:
+            break
+
+        windows.append(n + inp[i + 1] + inp[i + 2])
+
+    return sol1(windows)
+
+
+if __name__ == '__main__':
+    print(sol1(read()))
+    print(sol2(read()))
---
+++ 2021/02/main.py
@@ -0,0 +1,39 @@
+from util import read
+
+class Step:
+    def __init__(self, s):
+        self.d, self.x = s.split(' ')
+        self.x = int(self.x)
+
+def sol(inp):
+    depth = 0
+    pos = 0
+    for s in inp:
+        if s.d == 'forward':
+            pos += s.x
+        if s.d == 'down':
+            depth += s.x
+        if s.d == 'up':
+            depth -= s.x
+
+    return depth * pos
+
+def sol2(inp):
+    depth = 0
+    pos = 0
+    aim = 0
+    for s in inp:
+        if s.d == 'forward':
+            pos += s.x
+            depth += (aim * s.x)
+        if s.d == 'down':
+            aim += s.x
+        if s.d == 'up':
+            aim -= s.x
+
+    return depth * pos
+
+
+if __name__ == '__main__':
+    print(sol(read(Step)))
+    print(sol2(read(Step)))

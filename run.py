01c9dfe60fa70499194d81b39cfefea6a341a5c9
Sebastian Law <sebnlaw@gmail.com>
2021-12-10 05:13:06 -0800

    update runner

--- run.py
+++ run.py
@@ -1,17 +1,15 @@
 import asyncio
 import datetime
 import importlib
-import logging
 import sys
 import time
 import traceback
 from typing import Any, Literal, Optional
 
-from util import Input, SolutionFunction
-from util.web import now, get_session, URL_FMT
+from termcolor import cprint
 
-log = logging.getLogger()
-logging.basicConfig(level=logging.DEBUG)
+from util import Input, SolutionFunction
+from util.web import URL_FMT, get_session, now
 
 
 async def submit(day: int, year: int, level: Literal[1, 2], answer: Any):
@@ -39,22 +37,25 @@ def run_solution(solution: SolutionFunction, inp: Input, expected_result: Any = 
         name = solution.__name__
         length = after - before
 
-        log.debug(f'[ {name}{"[test]" if expected_result is not None else ""} finished in {length}ns ({length / 1000000}ms) {"({} seconds)".format(length / 1000000000) if length > 1000000000 else ""} ]')
+        cprint(f'[ {name}{"[test]" if expected_result is not None else ""} finished in {length}ns ({length / 1000000}ms) {"({} seconds)".format(length / 1000000000) if length > 1000000000 else ""} ]', 'blue')
         if expected_result is not None:
             passed = expected_result == answer
-            log.debug(f'[ {name} {"PASS" if passed else "FAIL"}: Expected {expected_result}, got {answer} ]')
+            cprint(f'[ {name} {"PASS" if passed else "FAIL"}: Expected {expected_result}, got {answer} ]', 'green' if passed else 'red')
             return passed
         else:
-            print(f'{name}: {answer}')
+            print(f'{name} answer: {answer}')
 
 
-async def main(day: int = None):
+async def main(day: int = None, year: int = None):
     if day is None:
         today = now()
     else:
         today = now().replace(day=day)
 
-    module = importlib.import_module(f'{today.year}.{today.day:02}.main')
+    if year is None:
+        year = today.year
+
+    module = importlib.import_module(f'{year}.{today.day:02}.main')
 
     try:
         p1: SolutionFunction = getattr(module, 'p1')
@@ -63,17 +64,17 @@ async def main(day: int = None):
         print("Solution functions not found")
         sys.exit(1)  # lol
 
-    with open(f'{today.year}/{today.day:02}/test') as f:
+    with open(f'{year}/{today.day:02}/test') as f:
         test_input: Input = Input(f)
 
-    with open(f'{today.year}/{today.day:02}/input') as f:
+    with open(f'{year}/{today.day:02}/input') as f:
         real_input: Input = Input(f)
 
     for i, f in enumerate((p1, p2), start=1):
         expected_result = getattr(f, '__aoc_test_expected_result__', None)
         if run_solution(f, test_input, expected_result):
             answer = run_solution(f, real_input)
-            # await submit(day=today.day, year=today.year, level=i, answer=answer)
+            # await submit(day=today.day, year=year, level=i, answer=answer)
 
 
 if __name__ == '__main__':
